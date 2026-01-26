4b2ba391e06c74c4e753dab5f96e8f36c125b07e
Jahanzaib190 <69195@student.buitms.edu.pk>
2026-01-27 03:21:22 +0500

    Set live API base URL

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = ''; // Because we are using the proxy
+export const BASE_URL = 'https://sportizzz.onrender.com';
 export const PRODUCTS_URL = '/api/products';
 export const USERS_URL = '/api/users';
 export const ORDERS_URL = '/api/orders';
